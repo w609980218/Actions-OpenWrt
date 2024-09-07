af66ac575f6adc6cfa8135757a6a03a168c2024d
w609980218 <35034693+w609980218@users.noreply.github.com>
2024-09-07 14:31:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #git clone https://github.com/MrH723/openwrt-packages.git package/openwrt-packages
-sed -i '$a src-git MrH723 https://github.com/openwrt-packages' feeds.conf.default
+sed -i '$a src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default
